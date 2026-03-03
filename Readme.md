3b628c5c8a5129865685d3f8b46b5a53f6243a89
r1ck7 <richims026@gmail.com>
2026-03-03 06:12:58 -0500

    U1

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# 260303
+
+## Pendientes
+
+0. Factores y data cleaning.
+1. Data del libro de multivariada (Análisis de componentes principales).
+2. Patrones de diseño con S7 (siempre comparar con C#).
+3. Librería en R con S7.
+4. Layout para diseños experimentales.
--- main.R
+++ main.R
@@ -1,1 +1,18 @@
+library(tidyverse)
+library(magrittr)
+library(ggplot2)
+library(haven)
+library(expss)
+library(gridExtra)
+
+#--------------------------------------------
+# Files
+#--------------------------------------------
+
+file.create("notes.md")
+file.create("Readme.md")
+
+#--------------------------------------------
+# Análisis de componentes principales
+#--------------------------------------------
 
